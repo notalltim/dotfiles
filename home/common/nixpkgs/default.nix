94823e0df5577daf276551ae3add733be7cd5fc5
Timothy Gallion <timbama@gmail.com>
2024-05-30 00:54:47 -0400

    remove overlay for now

--- home/common/nixpkgs/default.nix
+++ home/common/nixpkgs/default.nix
@@ -18,12 +18,13 @@ in {
       overlays = [self.inputs.nixgl.overlays.default self.inputs.fenix.overlays.default];
     };
     xdg.configFile."nixpkgs/config.nix".source = ./nixpkgs-config.nix;
-    xdg.configFile."nixpkgs/overlays/default.nix".text = ''
-      [
-        (builtins.getFlake "${self.inputs.nixgl}").overlays.default
-        (builtins.getFlake "${self.inputs.fenix}").overlays.default
-      ]
-
-    '';
+    # Not working gets a weird overlay must be a function
+    # xdg.configFile."nixpkgs/overlays/default.nix".text = ''
+    #   [
+    #     (builtins.getFlake "${self.inputs.nixgl}").overlays.default
+    #     (builtins.getFlake "${self.inputs.fenix}").overlays.default
+    #   ]
+    #
+    # '';
   };
 }
