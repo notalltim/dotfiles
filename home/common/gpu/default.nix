f255a6bb16e932ab3bd48d4dbed1e94ed902b3f1
Timothy Gallion <timbama@gmail.com>
2024-09-30 18:03:24 -0400

    Checkpoint: working module without nvidia

--- home/common/gpu/default.nix
+++ home/common/gpu/default.nix
@@ -1,6 +1,5 @@
 {
   lib,
-  self ? { }, # self is required for the impure version of nvidia support
   config,
   pkgs,
   ...
@@ -15,10 +14,13 @@ let
 in
 {
   options.baseline.gpu = {
-    enable = mkEnableOption "Enable GPU overlay";
-    enableVulkan = mkEnableOption "Vulkan support";
+    enable = mkEnableOption "Enable GPU options including nixgl for non nixos hosts";
+
+    enableVulkan = mkEnableOption "Vulkan support in nixgl and nvidia-offload (if enabled)";
+
     nvidia = {
       enable = mkEnableOption "Enable nvidia nixgl support";
+
       driverVersion = mkOption {
         type = nullOr str;
         default = null;
@@ -31,6 +33,7 @@ in
           The driver version can be gotten from `cat /proc/driver/nvidia/version`
         '';
       };
+
       driverHash = mkOption {
         type = nullOr str;
         default = null;
@@ -43,18 +46,34 @@ in
           The hash can be gotten by running `nix-prefetch-url https://download.nvidia.com/XFree86/Linux-x86_64/''${driverVersion}/NVIDIA-Linux-x86_64-''${driverVersion}.run"`
         '';
       };
+
+      nixGLPackages = mkOption {
+        type = nullOr str;
+        default = null;
+        example = "\${self}#legacyPackages.\${system}.nixgl";
+        description = ''
+          The location to pull nixgl from could be ''${self}#legacyPackages.''${system}.nixgl with the nixgl overlay applied or github:nix-community/nixGL#packages.''${system}.
+
+          NOTE: if you pull from the nixGL repo the run will require internet to eval. So nixgl-nvidia / nvidia-offload will not work without internet.
+        '';
+
+      };
     };
   };
 
   config = mkIf cfg.enable (
     let
-      bothNull = cfg.nvidia.driverHash == null && cfg.nvidia.driverVersion == null;
-      nietherNull = cfg.nvidia.driverHash != null && cfg.nvidia.driverVersion != null;
+      impureMode =
+        cfg.nvidia.driverHash == null
+        && cfg.nvidia.driverVersion == null
+        && cfg.nvidia.nixGLPackages != null;
+      pureMode = cfg.nvidia.driverHash != null && cfg.nvidia.driverVersion != null;
+
     in
     {
       assertions = [
         {
-          assertion = !cfg.nvidia.enable || bothNull || nietherNull;
+          assertion = !cfg.nvidia.enable || impureMode || pureMode;
           message = ''
             nvidia driver is enabled but the nvidia driver version (${
               if cfg.nvidia.driverVersion == null then "null" else cfg.nvidia.driverVersion
@@ -62,52 +81,38 @@ in
             and hash (${
               if cfg.nvidia.driverHash == null then "null" else cfg.nvidia.driverHash
             }) must both be null or both be non-null.
-            If they are null impure mode will be used otherwise pure eval will be used.
+
+            If they are null impure mode  nixGLPackages (${
+              if cfg.nvidia.nixGLPackages == null then "null" else cfg.nvidia.nixGLPackages
+            }) must be set must be non-null used otherwise pure eval will be used.
           '';
         }
         {
-          assertion = hasAttr "nixgl" pkgs;
+          assertion = !config.targets.genericLinux.enable || hasAttr "nixgl" pkgs;
           message = ''
-            nixgl is missing you need to include either the `overlays.nixgl` from nixgl
-            or `overlays.default` from notalltim's flake in the `nixpkgs.overlays` option.
-          '';
-        }
-        {
-          assertion = !cfg.nvidia.enable || bothNull && (hasAttr "outputs" self) || nietherNull;
-          message = ''
-            Nvidia is enabled in impure mode and the self has not been passed to `extraSpecialArgs`.          
-          '';
-        }
-        {
-          assertion =
-            !cfg.nvidia.enable || bothNull && (hasAttr "legacyPackages" self.outputs) || nietherNull;
-          message = ''
-            The legacyPackages is used to form a nix expression that is run in impure mode as part of the gpu wrapper script          
-          '';
-        }
-        {
-          assertion =
-            !cfg.nvidia.enable
-            || bothNull && (hasAttr "nixgl" self.outputs.legacyPackages.${pkgs.system})
-            || nietherNull;
-          message = ''
-            the nixgl overlay must be applied to `legacyPackages`.
+            nixgl is missing you need to include the `overlays.default` from nixgl. 
           '';
         }
       ];
+
       home = {
+        # Add helpers for running nixgl to PATH
         packages = [ pkgs.gpu-wrappers ];
-        activation = mkIf (cfg.nvidia.enable && bothNull) {
+        # In impure mode we clear the nixgl cache to ensure that the version is up to date
+        activation = mkIf (cfg.nvidia.enable && impureMode) {
           clearNixglCache = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
             [ -v DRY_RUN ] || rm -f ${config.xdg.cacheHome}/nixgl/result*
           '';
         };
       };
+
+      # overlay adds `gpu-wrappers` and ensures that nixgl is in pure mode if available
+      # mkAfter is needed because nixgl override needs to be at the end of the list overlays
       nixpkgs.overlays = mkAfter (
         [
-          (import ./overlay.nix { inherit self config lib; })
+          (import ./overlay.nix { inherit config lib; })
         ]
-        ++ optional (cfg.nvidia.enable && nietherNull) ((import ./nixgl.nix) cfg)
+        ++ optional (cfg.nvidia.enable && pureMode) ((import ./nixgl.nix) cfg)
       );
     }
   );
