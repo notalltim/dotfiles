d4484ff3db8eb12291cb4e05336507db08da281b
Timothy Gallion <timbama@gmail.com>
2026-01-18 10:48:18 -0500

    bugfix: remove auto-attach because it fials with multiple sessions

--- config/home/terminal/zellij.nix
+++ config/home/terminal/zellij.nix
@@ -7,8 +7,9 @@ in
   programs.zellij = mkIf cfg.enable {
     enable = true;
     enableFishIntegration = true;
-    exitShellOnExit = true;
-    attachExistingSession = true;
+    # See https://github.com/zellij-org/zellij/issues/3773 but this causes a softlock with multiple sessions
+    # exitShellOnExit = true;
+    # attachExistingSession = true;
     settings = {
       show_startup_tips = false;
     };
